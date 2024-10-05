c461b406989d261295663615f134143439be8422
alxbryann <bryanalexanderbogota@gmail.com>
2024-10-04 19:59:01 -0500

    Create profile pic

--- app/src/main/java/com/example/life2food/User.java
+++ app/src/main/java/com/example/life2food/User.java
@@ -4,14 +4,14 @@ public class User {
     private String firstName;
     private String lastName;
     private String email;
-    private String photo;
+    private String profileImage;
 
 
     public User(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-        this.photo = "https://firebasestorage.googleapis.com/v0/b/life2food-ec030.appspot.com/o/profile_images%2Fdefault_profile.png?alt=media&token=6caf9356-80ca-419d-b894-bd58d2878631";
+        this.profileImage = "https://firebasestorage.googleapis.com/v0/b/life2food-ec030.appspot.com/o/profile_images%2Fdefault_profile.png?alt=media&token=6caf9356-80ca-419d-b894-bd58d2878631";
     }
 
     // Getters y Setters
@@ -39,11 +39,11 @@ public class User {
         this.email = email;
     }
 
-    public String getPhoto() {
-        return photo;
+    public String getProfileImage() {
+        return profileImage;
     }
 
-    public void setPhoto(String photo) {
-        this.photo = photo;
+    public void setProfileImage(String profileImage) {
+        this.profileImage = profileImage;
     }
 }
