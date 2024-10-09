c980c969d1170ee080eb0391a169ccb03303cdcf
alxbryann <bryanalexanderbogota@gmail.com>
2024-10-09 15:48:08 -0500

    correct CartActivity

--- app/src/main/java/com/example/life2food/CartActivity.java
+++ app/src/main/java/com/example/life2food/CartActivity.java
@@ -14,6 +14,8 @@ import androidx.cardview.widget.CardView;
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -30,8 +32,8 @@ public class CartActivity extends AppCompatActivity {
     private String items = " ";
     private String cart = "";
     private LinearLayout linearLayout;
-    private int times = 0;
-    private String userId;
+    FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+    private String userId = String.valueOf(currentUser.getUid());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +50,7 @@ public class CartActivity extends AppCompatActivity {
         userCart = findViewById(R.id.cart);
 
         db.collection("carts")
+                .whereEqualTo("id_usuario", userId) //
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
@@ -73,113 +76,57 @@ public class CartActivity extends AppCompatActivity {
         String productQuantity;
         String productPrice;
         String image_url;
-        if (times == 0) {
-            items = items.substring(items.indexOf("id_usuario="));
-            userId = items.substring(11, items.indexOf(","));
-            items = items.substring(items.indexOf(","));
-            items = items.substring(items.indexOf("quantity="));
-            productQuantity = items.substring(9, items.indexOf(","));
-            items = items.substring(items.indexOf(","));
-            items = items.substring(items.indexOf("productId="));
-            productId = items.substring(10, items.indexOf(","));
-            items = items.substring(items.indexOf(","));
-            items = items.substring(items.indexOf("price="));
-            productPrice = items.substring(6, items.indexOf(","));
-            items = items.substring(items.indexOf(","));
-            items = items.substring(items.indexOf("imageUrl="));
-            image_url = items.substring(9, items.indexOf(","));
-            items = items.substring(items.indexOf(","));
-            items = items.substring(items.indexOf("productName="));
-            productName = items.substring(12, items.indexOf("}"));
-            items = items.substring(items.indexOf(","));
-            cart =   productName + "\n" +
-                    productQuantity + "\n" +
-                    productPrice +  "\n";
-            Button newDeleteButton = new Button(this);
-            newDeleteButton.setText("Eliminar producto del carrito");
-            newDeleteButton.setBackgroundColor(getResources().getColor(android.R.color.holo_red_light));
-            TextView newTextView = new TextView(this);
-            newTextView.setText(cart);
-            ImageView newImage = new ImageView(this);
-            Glide.with(this).load(image_url).override(350,350).into(newImage);
-            CardView cardView = new CardView(this);
-            cardView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-            cardView.setRadius(16);
-            cardView.setCardElevation(8);
-            LinearLayout innerLayout = new LinearLayout(this);
-            innerLayout.setOrientation(LinearLayout.VERTICAL);
-            innerLayout.setPadding(16, 16, 16, 16);
-            innerLayout.addView(newImage);
-            innerLayout.addView(newTextView);
-            innerLayout.addView(newDeleteButton);
-            cardView.addView(innerLayout);
-            newDeleteButton.setOnClickListener(v -> {
-                db.collection("products").document(productId).update("quantity", FieldValue.increment(Integer.parseInt(productQuantity)));
-                Map<String, Object> itemToRemove = new HashMap<>();
-                itemToRemove.put("imageUrl", image_url);
-                itemToRemove.put("price", Integer.parseInt(productPrice.substring(0, productPrice.indexOf("."))));
-                itemToRemove.put("productId", productId);
-                itemToRemove.put("productName", productName);
-                itemToRemove.put("quantity", 4);
-                removeItemFromCart(productId, itemToRemove);
-                linearLayout.removeView(cardView);
-            });
-            linearLayout.addView(cardView, linearLayout.indexOfChild(back));
-            cart = "";
-            times++;
-        } else {
-            items = items.substring(2);
-            items = items.substring(items.indexOf("quantity="));
-            productQuantity = items.substring(9, items.indexOf(","));
-            items = items.substring(items.indexOf(","));
-            items = items.substring(items.indexOf("productId="));
-            productId = items.substring(10, items.indexOf(","));
-            items = items.substring(items.indexOf(","));
-            items = items.substring(items.indexOf("price="));
-            productPrice = items.substring(6, items.indexOf(","));
-            items = items.substring(items.indexOf(","));
-            items = items.substring(items.indexOf("imageUrl="));
-            image_url = items.substring(9, items.indexOf(","));
-            items = items.substring(items.indexOf(","));
-            items = items.substring(items.indexOf("productName="));
-            productName = items.substring(12, items.indexOf("}"));
-            items = items.substring(items.indexOf("}"));
-            cart =   productName + "\n" +
-                    productQuantity + "\n" +
-                    productPrice +  "\n";
-            Button newDeleteButton = new Button(this);
-            newDeleteButton.setText("Eliminar producto del carrito");
-            newDeleteButton.setBackgroundColor(getResources().getColor(android.R.color.holo_red_light));
-            TextView newTextView = new TextView(this);
-            newTextView.setText(cart);
-            ImageView newImage = new ImageView(this);
-            Glide.with(this).load(image_url).override(350,350).into(newImage);
-            CardView cardView = new CardView(this);
-            cardView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-            cardView.setRadius(16);
-            cardView.setCardElevation(8);
-            LinearLayout innerLayout = new LinearLayout(this);
-            innerLayout.setOrientation(LinearLayout.VERTICAL);
-            innerLayout.setPadding(16, 16, 16, 16);
-            innerLayout.addView(newImage);
-            innerLayout.addView(newTextView);
-            innerLayout.addView(newDeleteButton);
-            cardView.addView(innerLayout);
-            newDeleteButton.setOnClickListener(v -> {
-                db.collection("products").document(productId).update("quantity", FieldValue.increment(Integer.parseInt(productQuantity)));
-                Map<String, Object> itemToRemove = new HashMap<>();
-                itemToRemove.put("imageUrl", image_url);
-                itemToRemove.put("price", Integer.parseInt(productPrice.substring(0, productPrice.indexOf("."))));
-                itemToRemove.put("productId", productId);
-                itemToRemove.put("productName", productName);
-                itemToRemove.put("quantity", 1);
-                removeItemFromCart(productId, itemToRemove);
 
-                linearLayout.removeView(cardView);
-            });
-            linearLayout.addView(cardView, linearLayout.indexOfChild(back));
-            cart = "";
-        }
+        items = items.substring(2);
+        items = items.substring(items.indexOf("quantity="));
+        productQuantity = items.substring(9, items.indexOf(","));
+        items = items.substring(items.indexOf(","));
+        items = items.substring(items.indexOf("productId="));
+        productId = items.substring(10, items.indexOf(","));
+        items = items.substring(items.indexOf(","));
+        items = items.substring(items.indexOf("price="));
+        productPrice = items.substring(6, items.indexOf(","));
+        items = items.substring(items.indexOf(","));
+        items = items.substring(items.indexOf("imageUrl="));
+        image_url = items.substring(9, items.indexOf(","));
+        items = items.substring(items.indexOf(","));
+        items = items.substring(items.indexOf("productName="));
+        productName = items.substring(12, items.indexOf("}"));
+        items = items.substring(items.indexOf("}"));
+        cart =   productName + "\n" +
+                productQuantity + "\n" +
+                productPrice +  "\n";
+        Button newDeleteButton = new Button(this);
+        newDeleteButton.setText("Eliminar producto del carrito");
+        newDeleteButton.setBackgroundColor(getResources().getColor(android.R.color.holo_red_light));
+        TextView newTextView = new TextView(this);
+        newTextView.setText(cart);
+        ImageView newImage = new ImageView(this);
+        Glide.with(this).load(image_url).override(350,350).into(newImage);
+        CardView cardView = new CardView(this);
+        cardView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        cardView.setRadius(16);
+        cardView.setCardElevation(8);
+        LinearLayout innerLayout = new LinearLayout(this);
+        innerLayout.setOrientation(LinearLayout.VERTICAL);
+        innerLayout.setPadding(16, 16, 16, 16);
+        innerLayout.addView(newImage);
+        innerLayout.addView(newTextView);
+        innerLayout.addView(newDeleteButton);
+        cardView.addView(innerLayout);
+        newDeleteButton.setOnClickListener(v -> {
+            db.collection("products").document(productId).update("quantity", FieldValue.increment(Integer.parseInt(productQuantity)));
+            Map<String, Object> itemToRemove = new HashMap<>();
+            itemToRemove.put("imageUrl", image_url);
+            itemToRemove.put("price", Integer.parseInt(productPrice.substring(0, productPrice.indexOf("."))));
+            itemToRemove.put("productId", productId);
+            itemToRemove.put("productName", productName);
+            itemToRemove.put("quantity", Integer.parseInt(productQuantity));
+            removeItemFromCart(productId, itemToRemove);
+            linearLayout.removeView(cardView);
+        });
+        linearLayout.addView(cardView, linearLayout.indexOfChild(back));
+        cart = "";
     }
 
     private void removeItemFromCart(String productId, Map<String, Object> itemToRemove) {
